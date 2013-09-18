b51f085da70a06038fd0fc1d2f16a74274eceee5
Ian Mac <webguyian@gmail.com>
2013-09-18 19:54:23 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IndiegroveAwesome::Application.config.secret_key_base = '2dbea95b60eb6b734519e6002314bfc7bbbe01e04a2d8ccedd186c293b1c2a6a4d0a0e1cb2ca3a223e8948367947d2faa7d8eae7bb4208a5620d9657f227a075'
